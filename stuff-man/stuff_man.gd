db1340fd149fc3857916286eb6b13f2eb0668f07
kidnamedfinger1 <111032514+kidnamedfinger1@users.noreply.github.com>
2024-11-19 10:30:02 -0600

    Add files via upload

--- stuff-man/stuff_man.gd
+++ stuff-man/stuff_man.gd
@@ -1,11 +1,26 @@
 extends CharacterBody2D
 
+# Variables
+@export var speed: float = 200        # Movement speed
+@export var jump_force: float = 400   # Jump strength
+@export var gravity: float = 800      # Gravity applied to the character
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _physics_process(delta: float):
+	# Apply gravity
+	if not is_on_floor():
+		velocity.y += gravity * delta
 
+	# Handle horizontal movement
+	var input_direction = 0
+	if Input.is_action_pressed("ui_left"):
+		input_direction -= 1
+	if Input.is_action_pressed("ui_right"):
+		input_direction += 1
+	velocity.x = input_direction * speed
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	# Handle jumping
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = -jump_force
+
+	# Move the character
+	move_and_slide()
