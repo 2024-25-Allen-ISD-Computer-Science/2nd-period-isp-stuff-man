e9e8154c04e3bb632aa95ab73dfc07d698cc5e4c
peytonboudreaux <111151244+peytonboudreaux@users.noreply.github.com>
2024-12-03 10:24:05 -0600

    Add files via upload

--- stuff-man/stuff_man.gd
+++ stuff-man/stuff_man.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 # Variables
-@export var speed: float = 200        # Movement speed
+@export var speed: float = 400        # Movement speed
 @export var jump_force: float = 400   # Jump strength
 @export var gravity: float = 800      # Gravity applied to the character
 
@@ -19,7 +19,7 @@ func _physics_process(delta: float):
 	velocity.x = input_direction * speed
 
 	# Handle jumping
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+	if Input.is_action_pressed("ui_up") and is_on_floor():
 		velocity.y = -jump_force
 
 	# Move the character
