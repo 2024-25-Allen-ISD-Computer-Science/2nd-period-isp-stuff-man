e54ccac71cebb1f22ac1e6bdd0a798726055b63a
kidnamedfinger1 <111032514+kidnamedfinger1@users.noreply.github.com>
2025-04-03 10:54:15 -0500

    Add files via upload

--- stuff-man/money.gd
+++ stuff-man/money.gd
@@ -1,11 +1,14 @@
-extends Node
+extends StaticBody2D
 
-var money: float = 0.00
+@export var value: float = 0.01
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
-
+		
+func get_cash():
+	print("got $" + str(value))
+	Globalvars.money += value
+	queue_free()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
